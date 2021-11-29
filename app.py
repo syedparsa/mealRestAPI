1fd2dcb41fc1a320f6724471bfa89070d872d5e5
Muhammad Zeeshan <syedzeshan786512@gmail.com>
2021-11-29 12:26:38 +0100

    extra files removed from app

--- app.py
+++ app.py
@@ -10,12 +10,13 @@ from functools import wraps
 
 
 app = Flask(__name__)
-app.config["DATABASE_URL"] ='postgres://khbjjuusbhztzo:6e3ffe89e02363413236a1a658cada7dc5e50e1c5b120a1be8c39a33d621582a@ec2-34-241-19-183.eu-west-1.compute.amazonaws.com:5432/dmuftpqtlgggo'
+app.config["DATABASE_URL"] = 'postgres://khbjjuusbhztzo:6e3ffe89e02363413236a1a658cada7dc5e50e1c5b120a1be8c39a33d621582a@ec2-34-241-19-183.eu-west-1.compute.amazonaws.com:5432/dmuftpqtlgggo'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 app.config['SECRET_KEY'] = 'Th1s1ss3cr3t'
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
 
 class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -33,8 +34,11 @@ class Meals (db.Model):
     isVegan = db.Column(db.Boolean, default=False)
     isGlutenFree = db.Column(db.Boolean, default=False)
     description = db.Column(db.String(500))
+
+
 db.create_all()
 db.session.commit()
+
 
 def token_required(is_admin):
     def decorator(f):
@@ -46,27 +50,40 @@ def token_required(is_admin):
             if not token:
                 return error_handler(400, "Valid token is missing")
             try:
-                data = jwt.decode(token,app.config["SECRET_KEY"], algorithms=["HS256"])
+                data = jwt.decode(
+                    token,
+                    app.config["SECRET_KEY"],
+                    algorithms=["HS256"])
                 current_user = Users.query.filter_by(id=data['id']).first()
                 if is_admin:
                     if not current_user.admin:
-                        return error_handler(401, "Unauthorized Access!") 
-            except BaseException :
+                        return error_handler(401, "Unauthorized Access!")
+            except BaseException:
                 return error_handler(404, "User not found!")
             return f(*args, **kwargs)
         return wrapper
     return decorator
 
-def responce_handler(status_code,message_header,message,additional_message=None):
+
+def responce_handler(
+        status_code,
+        message_header,
+        message,
+        additional_message=None):
     if additional_message:
-        return jsonify({'staus': status_code, message_header: message, 'note': additional_message})
+        return jsonify({'staus': status_code,
+                        message_header: message,
+                        'note': additional_message})
     return jsonify({'staus': status_code, message_header: message})
+
 
 def success_handler(message):
     return responce_handler(200, "message", message)
 
-def error_handler(status_code,message):
+
+def error_handler(status_code, message):
     return responce_handler(status_code, "error", message)
+
 
 @app.route('/register', methods=['POST'])
 def signup_user():
@@ -74,77 +91,83 @@ def signup_user():
     password = data['password']
     name = data['name']
     if password.strip() is None or name.strip() is None:
-        return error_handler(400,"Invalid Input!")
+        return error_handler(400, "Invalid Input!")
     if Users.query.filter_by(name=name).first() is not None:
         return error_handler(403, "User already exists!")
 
     hashed_password = generate_passworpid_hash(password, method='sha256')
 
-    new_user = Users( name=name, password=hashed_password, admin=False) 
+    new_user = Users(name=name, password=hashed_password, admin=False)
     db.session.add(new_user)
     db.session.commit()
 
     return success_handler("registered successfully")
 
-@app.route('/login', methods=['GET', 'POST'])  
-def login_user():
-    auth = request.authorization   
-    if not auth or not auth.username or not auth.password:  
-        return error_handler(401,'Username or password is incorrect') 
 
-    user = Users.query.filter_by(name=auth.username).first()   
-     
-    if check_password_hash(user.password, auth.password):  
-       token = jwt.encode({'id': user.id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)},app.config["SECRET_KEY"], algorithm="HS256")
-       return responce_handler(200, "token",token) 
+@app.route('/login', methods=['GET', 'POST'])
+def login_user():
+    auth = request.authorization
+    if not auth or not auth.username or not auth.password:
+        return error_handler(401, 'Username or password is incorrect')
+
+    user = Users.query.filter_by(name=auth.username).first()
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'id': user.id, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(minutes=30)}, app.config["SECRET_KEY"], algorithm="HS256")
+        return responce_handler(200, "token", token)
     return error_handler(404, "User Not Found")
+
 
 @app.route('/users', methods=['GET'])
 @token_required(is_admin=True)
 def get_all_users():
 
-    users = Users.query.all() 
+    users = Users.query.all()
     result = []
-    for user in users:   
-        user_data = {}   
-        user_data['id'] = user.id   
-        user_data['name'] = user.name 
-        user_data['admin'] = user.admin 
-        result.append(user_data)   
-    return responce_handler(200,"users",result)
+    for user in users:
+        user_data = {}
+        user_data['id'] = user.id
+        user_data['name'] = user.name
+        user_data['admin'] = user.admin
+        result.append(user_data)
+    return responce_handler(200, "users", result)
+
 
 @app.route('/')
 def index():
     return success_handler("Welcome to Meals REST API")
 
+
 @app.route('/createMeal', methods=['POST'])
 @token_required(is_admin=True)
 def create_Meal():
-   
-   data = request.get_json() 
-   name = data['name']
-   price = data['price']
-   ingredients = data['ingredients']
-   isSpicy = data['isSpicy']
-   isVegan = data['isVegan']
-   isGlutenFree= data['isGlutenFree']
-   description= data['description']
 
-   new_Meal = Meals(
-       name=name,
-       price=price,
-       ingredients=ingredients,
-       isSpicy=isSpicy,
-       isVegan=isVegan,
-       isGlutenFree=isGlutenFree,
-       description=description
-       )  
-   db.session.add(new_Meal)   
-   db.session.commit()   
+    data = request.get_json()
+    name = data['name']
+    price = data['price']
+    ingredients = data['ingredients']
+    isSpicy = data['isSpicy']
+    isVegan = data['isVegan']
+    isGlutenFree = data['isGlutenFree']
+    description = data['description']
 
-   return success_handler("new meal added!")
+    new_Meal = Meals(
+        name=name,
+        price=price,
+        ingredients=ingredients,
+        isSpicy=isSpicy,
+        isVegan=isVegan,
+        isGlutenFree=isGlutenFree,
+        description=description
+    )
+    db.session.add(new_Meal)
+    db.session.commit()
 
-@app.route('/meal',methods=['GET'])
+    return success_handler("new meal added!")
+
+
+@app.route('/meal', methods=['GET'])
 @token_required(is_admin=False)
 def get_Meals():
     all_meals = Meals.query.all()
@@ -152,7 +175,7 @@ def get_Meals():
     for meal in all_meals:
         eat_data = get_meal_details(meal)
         result.append(eat_data)
-    return responce_handler(200,"meals", result)
+    return responce_handler(200, "meals", result)
 
 
 def get_meal_details(meal):
@@ -166,32 +189,43 @@ def get_meal_details(meal):
             'description': meal.description,
             }
 
-@app.route('/updateMeal/<id>',methods=['POST'])
+
+@app.route('/updateMeal/<id>', methods=['POST'])
 @token_required(is_admin=True)
 def update(id):
-    meal=Meals.query.filter_by(mealId=id)
-    data=request.get_json()
+    meal = Meals.query.filter_by(mealId=id)
+    data = request.get_json()
     updateMeal(meal, data)
     if 'name' in data.keys():
-        return responce_handler(200, 'message','Meal Updated',additional_message="Meal's name cannot be updated!\nDelete and create new one if you want!!")
+        return responce_handler(
+            200,
+            'message',
+            'Meal Updated',
+            additional_message="Meal's name cannot be updated!\nDelete and create new one if you want!!")
     return success_handler("Meal updated!")
 
-@app.route('/updateMeal/<name>',methods=['POST'])
+
+@app.route('/updateMeal/<name>', methods=['POST'])
 @token_required(is_admin=False)
 def update_by_name(name):
-    meal=Meals.query.filter_by(name=name)
-    data=request.get_json()
+    meal = Meals.query.filter_by(name=name)
+    data = request.get_json()
     updateMeal(meal, data)
     if 'name' in data.keys():
-        return responce_handler(200, 'message','Meal Updated',additional_message="Meal's name cannot be updated!\nDelete and create new one if you want!!")
+        return responce_handler(
+            200,
+            'message',
+            'Meal Updated',
+            additional_message="Meal's name cannot be updated!\nDelete and create new one if you want!!")
     return success_handler("Meal updated!")
+
 
 def updateMeal(meal, data):
     if meal:
         for d in data.keys():
-            if d=='price':
+            if d == 'price':
                 meal.price = data[d]
-            elif d =='ingredients':
+            elif d == 'ingredients':
                 meal.ingredients = data[d]
             elif d == 'isSpicy':
                 meal.isSpicy = data[d]
@@ -203,7 +237,8 @@ def updateMeal(meal, data):
                 meal.description = data[d]
         db.session.commit()
 
-@app.route('/meal/<id>',methods=['GET'])
+
+@app.route('/meal/<id>', methods=['GET'])
 @token_required(is_admin=False)
 def get_FMeals(id):
     meal = Meals.query.filter_by(mealId=id)
@@ -212,15 +247,16 @@ def get_FMeals(id):
     return responce_handler(200, "meal", meal)
 
 
-@app.route('/meal/<id>',methods=['DELETE'])
+@app.route('/meal/<id>', methods=['DELETE'])
 @token_required(is_admin=True)
 def delete_Meals(id):
     meal = Meals.query.filter_by(mealId=id)
     if meal:
         meal.delete()
-        db.session.commit() 
+        db.session.commit()
         return success_handler("Deleted successfuly")
-    return error_handler(404, "Meal Not Found!!")   
+    return error_handler(404, "Meal Not Found!!")
 
-    
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
